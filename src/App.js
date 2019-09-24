27d969f7624a0a8cf2e337f421b44a491ba853d5
Erika Miwa <erikamiwa612@gmail.com>
2019-09-24 12:11:55 -0700

    add 3 pages with react router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Explore from './components/Explore/Explore';
+import Feedback from './components/Feedback/Feedback';
+import Navbar from './components/Navbar/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/explore' component={Explore} />
+            <Route path='/feedback' component={Feedback} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link, NavLink, withRouter } from 'react-router-dom'
+
+const Navbar = (props) => {
+  return (
+    <nav>
+      <div className="Navbar">
+        <Link className="brand-logo" to="/">Revision.io</Link>
+          <ul>
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to='/explore'>Explore</NavLink></li>
+            <li><NavLink to='/feedback'>Feedback</NavLink></li>
+        </ul>
+      </div>
+    </nav> 
+  )
+}
+
+export default withRouter(Navbar)
