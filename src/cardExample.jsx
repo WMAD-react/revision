98380f75ade6219aace8340e6a372268dfd04e76
Ayako <ayakosayama1987@gmail.com>
2019-09-25 11:18:18 -0700

    added extra conditions for card example for storybook

--- src/cardExample.jsx
+++ src/cardExample.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const CardExpamle = ({ title, text }) => {
+const CardExpamle = ({ title, text, image }) => {
   return (
     <div className="card" style={{ width: "300px", padding: "20px", border: '1px solid #333' }}>
-      <img src="https://ichef.bbci.co.uk/news/660/cpsprodpb/3DAD/production/_104898751_gettyimages-844466808.jpg" style={{ width: "100%" }} alt=""/>
+      
+      {image ? <img src={image} style={{ width: '100%'}} alt=""/>
+        : <div style={{ background: 'gray', width: '100%', height: '200px' }}>No image</div>
+      }
       <div className="cardInner">
         <div className="cardTitle" style={{ marginBottom: '20px', fontSize: '20px', fontWeight: 'bold' }}>{title}</div>
         <div className="cardText">{text}</div>
